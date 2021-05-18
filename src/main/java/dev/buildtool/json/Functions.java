51b11d1d5bec28fa2ecfb93c8f4ac59b9b03cbd5
alexiy <alexiy.ov@gmail.com>
2021-05-18 07:40:01 +0300

    Delete bugged parser

--- src/main/java/dev/buildtool/json/Functions.java
+++ src/main/java/dev/buildtool/json/Functions.java
@@ -14,11 +14,11 @@ public class Functions {
     }
 
     static boolean isIdStartChar(char c) {
-        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '$' || c == '_') || Json5ParserBugged.idStart.matcher(String.valueOf(c)).matches();
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '$' || c == '_') || Json5Parser.ID_START.matcher(String.valueOf(c)).matches();
     }
 
     static boolean isIdContinueChar(char c) {
-        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || (c == '$') || (c == '_') || (c == '\u200C') || (c == '\u200D') || Json5ParserBugged.idContinue.matcher(String.valueOf(c)).matches();
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || (c == '$') || (c == '_') || (c == '\u200C') || (c == '\u200D') || Json5Parser.ID_CONTINUE.matcher(String.valueOf(c)).matches();
     }
 
     /**
